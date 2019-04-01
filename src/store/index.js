df5d06e2455018ad628a6956daf27b5a84889c86
Andrew Zappella <andrew.zappella@gmail.com>
2019-04-01 15:09:23 +0200

    Added production flag for redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 import { loadState, saveState } from './local-storage';
 
 const initialState = loadState();
-const middleware = applyMiddleware(thunk, logger);
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = process.env.NODE_ENV !== 'production' ?
+    applyMiddleware(thunk, logger) :
+    applyMiddleware(thunk);
+
+// const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
     initialState,
-    reduxDevTools(
-        middleware
-    )
+    middleware
 );
 
 store.subscribe(() => {
