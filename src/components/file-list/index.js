aa95058a3635f2f75b249ba4684c9f2cd1dca856
Andrew Zappella <andrew.zappella@gmail.com>
2019-05-17 12:02:08 +0200

    Updated style for the grant input in file-list

--- src/components/file-list/index.js
+++ src/components/file-list/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import ReactTable from 'react-table';
 
-const FileList = ({ user, files, showModal, grantAccess}) => {
-    const [toAccount, setToAccount] = useState('');
+const FileList = ({ user, files, grantAccess}) => {
     const columns = [{
         Header: 'Filename',
-        accessor: 'filename' // String-based value accessors!
+        accessor: 'filename', // String-based value accessors!
     }, {
         Header: 'Meta',
         accessor: 'meta',
@@ -15,24 +14,28 @@ const FileList = ({ user, files, showModal, grantAccess}) => {
     }, {
         Header: 'Actions',
         accessor: 'acl',
-        Cell: (row) => (
-            <form
-                onSubmit={(e) => {
-                    const grant = {
-                        acl: row.value,
-                        ownerAccount: user.account,
-                        password: user.password,
-                        toAccount,
-                        permissionType: 'read'
-                    };
-                    grantAccess(grant);
-                    e.preventDefault();
-                }}
-            >
-                <input type='text' onChange={(e) => setToAccount(e.target.value)} value={toAccount} />
-                <button type='submit'>Grant</button>
-            </form>
-        )
+        width: 350,
+        Cell: (row) => {
+            const [toAccount, setToAccount] = useState('');
+            return (
+                <form
+                    onSubmit={(e) => {
+                        const grant = {
+                            acl: row.value,
+                            ownerAccount: user.account,
+                            password: user.password,
+                            toAccount,
+                            permissionType: 'read'
+                        };
+                        grantAccess(grant);
+                        e.preventDefault();
+                    }}
+                >
+                    <button type='submit'>Grant To</button>
+                    <input name='acl' type='text' onChange={(e) => setToAccount(e.target.value)} value={toAccount} />
+                </form>
+            );
+        }
     }];
 
     return <ReactTable
